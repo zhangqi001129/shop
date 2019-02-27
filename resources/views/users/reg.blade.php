f2585b7e8e1cda8a28b6d63819e47db40b92356b
zhangqi001129 <2843275748@qq.com>
2019-02-27 11:36:27 +0800

    zhifu

--- resources/views/users/reg.blade.php
+++ resources/views/users/reg.blade.php
@@ -8,11 +8,7 @@
         <h2 class="form-signin-heading">用户注册</h2>
         <label for="inputNickName">Nickname</label>
         <input type="text" name="nick_name" id="inputNickName" class="form-control" placeholder="nickname" required autofocus>
-
-        <label for="inputAge">age</label>
-        <input type="text" name="age" id="inputAge" class="form-control" placeholder="nickname" required autofocus>
-
-
+        
         <label for="inputEmail">Email</label>
         <input type="email" name="email" id="inputEmail" class="form-control" placeholder="@" required autofocus>
 
