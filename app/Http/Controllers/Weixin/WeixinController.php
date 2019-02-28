8f98a2a813d3eec8dbd2358e12988fb28e95659b
zhangqi001129 <2843275748@qq.com>
2019-02-28 06:53:50 +0000

    微信登录

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -457,4 +457,43 @@ class WeixinController extends Controller
         }
         return $arr;
     }
+    /**
+     * 微信登录测试
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function login()
+    {
+        $myurl="https:mall.77sc.com.cn/weixin.php?rl=http://zq.lixiaonitongxue.top/weixin/getcode";
+        $data=[
+            'url'=>'https://open.weixin.qq.com/connect/qrconnect?appid=wxe24f70961302b5a5&redirect_uri='.$myurl.'&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect'
+        ];
+        return view('weixin.login',$data);
+    }
+
+    /**
+     * 接收code
+     */
+    public function getCode()
+    {
+        // 1 回调拿到 code (用户确认登录后 微信会跳 redirect )
+        $code = $_GET['code'];          // code
+        //2 用code换取access_token 请求接口
+
+        $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxe24f70961302b5a5&secret=0f121743ff20a3a454e4a12aeecef4be&code='.$code.'&grant_type=authorization_code';
+        $token_json = file_get_contents($token_url);
+        $token_arr = json_decode($token_json,true);
+        echo '<hr>';
+        echo '<pre>';print_r($token_arr);echo '</pre>';
+
+        $access_token = $token_arr['access_token'];
+        $openid = $token_arr['openid'];
+
+        // 3 携带token  获取用户信息
+        $user_info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+        $user_json = file_get_contents($user_info_url);
+
+        $user_arr = json_decode($user_json,true);
+        echo '<hr>';
+        echo '<pre>';print_r($user_arr);echo '</pre>';
+        }
 }
