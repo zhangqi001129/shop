2b07d2ebfbcbbcc2553ae08bad570e314ae7d561
zhangqi <zhangqi@localhost.localdomain>
2019-03-13 03:17:21 +0000

    发布

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -62,4 +62,15 @@ class TestController extends Controller
 
 
 	}
+
+	public function fabu(){
+        $server_name=[
+            '192.168.131.29',
+        ];
+        foreach($server_name as $k=>$v){
+            $cmd= 'ssh '.$v .' " cd /home/wwwroot/fabu ; git pull" ';
+            echo $cmd;echo '<hr>';
+            $result= shell_exec($cmd);
+        }
+    }
 }
